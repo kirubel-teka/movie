714cd71b8aa03f061c69f5c4b8294674c1ae70a7
kirubel-teka <se.kirubel.teka@gmail.com>
2025-05-05 15:31:04 +0300

    slider animation added

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//---Slider
+let slideIndex = 1;
+window.plusSlides = (n) => {
+  showSlides((slideIndex += n));
+};
+
+window.currentSlide = (n) => {
+  showSlides((slideIndex = n));
+};
+
+const showSlides = (n) => {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+};
+
+showSlides(slideIndex);
+
+// Run every 3 seconds
+setInterval(() => {
+  plusSlides(1);
+}, 3000);
+
+//Slider---
+
+//---Custom Scrollbar
+//Trending
